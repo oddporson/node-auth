881b95547214655b254c770e24c91c10245a03b4
Porson <porsonlee@gmail.com>
2020-03-21 14:25:59 -0700

    left TODO comment to resolve this issue

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,14 +1,13 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
-  const token = req.header('auth-token');
+  const token = req.header('auth-token'); // TODO: Auth token couldn't verify same hashed password in POSTMAN application
   if(!token) return res.status(401).send('Access Denied');
-
   try{
     const verified = jwt.verifiy(token, process.env.TOKEN_SECRET);
     req.user = verified;
     next();
-  }catch(err){
+  }catch (err) {
     res.status(400).send('Invalid Token');
   }
 }
