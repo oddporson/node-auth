ea823e087405b1ed896436c185a8e1e0fa8319e2
Porson <porsonlee@gmail.com>
2020-03-21 14:26:39 -0700

    created router get request for json of a post object

--- routes/posts.js
+++ routes/posts.js
@@ -6,7 +6,7 @@ router.get('/', verify, (req,res) => {
   res.json({
     posts: {
       title: 'my first post', 
-      description: 'random data you shouldnt access'
+      description: 'random data you shouldnt access without being logged in'
     }
   });
 });
