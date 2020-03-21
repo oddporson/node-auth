7cb4238a6ccde26c48e502f0841fb133e976de74
Porson <porsonlee@gmail.com>
2020-03-21 00:00:05 -0700

    added conditional that check if email already exists

--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,11 @@ router.post('/register', async (req, res) => {
   const { error } = registerValidation(req.body);
     if(error) return res.status(400).send(error.details[0].message);
 
+    // Checking if the user is already in the database
+    const emailExist = await User.findOne({email: req.body.email});
+    if(emailExist) return res.status(400).send('Email already exisits');
+
+    // Create a new user
     const user = new User({
       name: req.body.name,
       email: req.body.email,
