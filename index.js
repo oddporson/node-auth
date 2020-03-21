2f1794f1aefc6c7beb175201bd665a14a153ace4
Porson <porsonlee@gmail.com>
2020-03-20 22:17:08 -0700

    reorder section of codes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+// Import Routes
+const authRoute = require('./routes/auth');
 
 dotenv.config();
 
@@ -10,11 +12,10 @@ mongoose.connect(
   process.env.DB_CONNECT, { useUnifiedTopology: true, useNewUrlParser: true }, () => console.log('Connected to db!')
 );
 
-// Import Routes
-const authRoute = require('./routes/auth');
+// Middleware
+app.use(express.json());
 
-
-// Route Middleware
+// Route Middlewares
 app.use('/api/user', authRoute);
 
 
