9a2d78a9de5fee6db60d6c46566a66cd871ce726
Porson <porsonlee@gmail.com>
2020-03-20 19:45:36 -0700

    installed dotenv and created env to store personal password into there

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+
+// Connect to DB
+mongoose.connect(
+  process.env.DB_CONNECT, 
+{ useUnifiedTopology: true, useNewUrlParser: true },
+() => console.log('Connected to db!')
+);
 
 // Import Routes
 const authRoute = require('./routes/auth');
